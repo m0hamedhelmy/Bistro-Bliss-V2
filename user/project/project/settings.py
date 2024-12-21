d0d6d91f33c7769a6099d400a2cad91468ee6590
m0hamedhelmy <119460476+m0hamedhelmy@users.noreply.github.com>
2024-12-21 20:50:30 +0200

    Update Data base in settings.py

--- user/project/project/settings.py
+++ user/project/project/settings.py
@@ -80,10 +80,15 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bistrobliss_db',
+        'USER': 'mohamedhelmy2004@gmail.com',
+        'PASSWORD': 'MohamedProjectv2_',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
